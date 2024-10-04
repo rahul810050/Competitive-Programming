e7d61b738c69e938ed336872e6265c0e5202c964
Rahul Naskar <rahul9875458880@gmail.com>
2024-10-04 19:49:58 +0530

    questions solved

--- 1986D.cpp
+++ 1986D.cpp
@@ -4,12 +4,17 @@
 
 #define ll long long
 #define forn(i, n) for(int i = 0; i < n; i++)
+#define for1(i, n) for(int i = 1; i <= n; i++)
+#define foru(i, n) for(int i = n-1; i >= 0; i--)
 #define fi first
 #define se second
 #define all(x) (x).begin(),(x).end()
+#define allp(x) (x).begin(), (x).begin() + (x).size()/2, (x).rbegin()
 #define pb push_back
-#define out(ans) cout << ans << endl
-#define yn(a) cout << (a ? "YES" : "NO") << endl
+#define ppb pop_back
+#define read(a) for(auto &i: a) cin >> i
+#define out(ans) cout<<ans<<endl
+#define yn(a) cout<< (a ? "YES": "NO") <<endl
 
 using namespace std;
 
@@ -17,53 +22,62 @@ typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef double dl;
 
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     int t;
-    cin >> t;
-    while (t--) {
+    cin>>t;
+    while(t--){
         int n;
-        cin >> n;
-
         string s;
-        cin >> s;
+        cin>>n>>s;
 
-        if (s[0] == '0') {
-            cout << 0 << endl;
-        } else if (n == 2) {
-            cout << s << endl;
-        } else if (n == 3) {
-            ll mn_sum = 0;
-            if(s[1] == '0') {
-							int x = s[0] - '0'; 
-							int y = stoi(s.substr(1, 2)); 
-							int p1 = min((x + y), (x * y));
-							int a = s[2] - '0';
-							int b = stoi(s.substr(0,1));
-							int p2 = min((a + b), (a * b));
-							cout<<min(p1,p2)<<endl;
-					  }
+        if(n == 2) {
+            cout << stoi(s) << endl;  
+        }
+        else if(n == 3){
+            if(s[0] == '0' || s[2] == '0'){  
+                cout << 0 << endl;  
+            }
             else{
-							int x = s[0] - '0'; 
-							int y = stoi(s.substr(1, 2)); 
-							int p1 = min((x + y), (x * y));
-							int a = s[2] - '0';
-							int b = stoi(s.substr(0,1));
-							int p2 = min((a + b), (a * b));
-							cout<<min(p1,p2);
-						}
-        } else if (n > 3) {
-            if (s.find('0') == string::npos) { 
-                ll mn_sum = 0;
-                for (int i = 0; i < n; i++) {
-                    ll addition = mn_sum + (s[i] - '0');
-                    ll multiplication = mn_sum * (s[i] - '0');
-                    mn_sum = min(addition, multiplication);
+                int str1 = stoi(s.substr(0,2));
+                int str2 = stoi(s.substr(1,2));
+
+                int mini1 = min(str1 + (s[2] - '0'), str1 * (s[2] - '0'));
+                int mini2 = min(str2 + (s[0] - '0'), str2 * (s[0] - '0'));
+
+                cout << min(mini1, mini2) << endl;
+            }
+        }
+        else{
+            int mn = *min_element(all(s)) - '0';
+
+            if(mn == 0) {
+                cout << 0 << endl;  
+            }
+            else {
+                vector<pair<int,int>> v;
+                vll totals;
+
+                for (int i = 0; i < n-1; i++) {
+                    v.pb({stoi(s.substr(i, 2)), i});
                 }
-                cout << mn_sum << endl;
-            } \
+
+                for (int i = 0; i < v.size(); i++) {
+                    ll each_sum = v[i].fi;
+                    int sec = v[i].se;
+
+                    for (int j = 0; j < n; j++) {
+                        if((j != sec) && (j != sec+1) && s[j] != '1'){
+                            each_sum += (s[j] - '0');  
+                        }
+                    }
+                    totals.pb(each_sum); 
+                }
+
+                cout << *min_element(all(totals)) << endl;
+            }
         }
     }
     return 0;
