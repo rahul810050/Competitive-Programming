6ffdc81e23045480f87339a9b010a1596c2d40f7
Rahul Naskar <rahul9875458880@gmail.com>
2024-12-03 23:09:42 +0530

    added some  changges

--- Algo Cup/daa.cpp
+++ Algo Cup/daa.cpp
@@ -1,42 +1,48 @@
-#include <iostream>
-#include <vector>
-#include <limits.h>
+// hamiltonian   cycle
 
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// #include <limits.h>
 
-#define V 4
+// using namespace std;
 
-int tsp(int graph[][V], vector<bool>& visited, int currPos, int n, int count, int cost, int& ans) {
-    if (count == n && graph[currPos][0]) {
-        ans = min(ans, cost + graph[currPos][0]);
-        return ans;
-    }
+// #define V 4
 
-    for (int i = 0; i < n; i++) {
-        if (!visited[i] && graph[currPos][i]) {
-            visited[i] = true;
-            tsp(graph, visited, i, n, count + 1, cost + graph[currPos][i], ans);
-            visited[i] = false;
-        }
-    }
-    return ans;
-}
+// int tsp(int graph[][V], vector<bool>& visited, int currPos, int n, int count, int cost, int& ans) {
+//     if (count == n && graph[currPos][0]) {
+//         ans = min(ans, cost + graph[currPos][0]);
+//         return ans;
+//     }
 
-int main() {
-    int graph[V][V] = {
-        {0, 10, 15, 20},
-        {10, 0, 35, 25},
-        {15, 35, 0, 30},
-        {20, 25, 30, 0}
-    };
+//     for (int i = 0; i < n; i++) {
+//         if (!visited[i] && graph[currPos][i]) {
+//             visited[i] = true;
+//             tsp(graph, visited, i, n, count + 1, cost + graph[currPos][i], ans);
+//             visited[i] = false;
+//         }
+//     }
+//     return ans;
+// }
 
-    vector<bool> visited(V, false);
-    visited[0] = true;
-    int ans = INT_MAX;
+// int main() {
+//     int graph[V][V] = {
+//         {0, 10, 15, 20},
+//         {10, 0, 35, 25},
+//         {15, 35, 0, 30},
+//         {20, 25, 30, 0}
+//     };
 
-    ans = tsp(graph, visited, 0, V, 1, 0, ans);
+//     vector<bool> visited(V, false);
+//     visited[0] = true;
+//     int ans = INT_MAX;
 
-    cout << "The minimum cost of the Hamiltonian Cycle is " << ans << endl;
+//     ans = tsp(graph, visited, 0, V, 1, 0, ans);
 
-    return 0;
-}
+//     cout << "The minimum cost of the Hamiltonian Cycle is " << ans << endl;
+
+//     return 0;
+// }
+
+
+
+
