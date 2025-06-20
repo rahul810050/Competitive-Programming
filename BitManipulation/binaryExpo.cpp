6a775379b806ca64dad128aa167a9410b1da6205
rahul810050 <rahul810050@gmail.com>
2025-06-20 12:13:51 +0530

    binary exponentiations

--- BitManipulation/binaryExpo.cpp
+++ BitManipulation/binaryExpo.cpp
@@ -1,13 +1,13 @@
-#include<bits/stdc++.h>
-using namespace std;
-const int MOD = 1e4+7;
+// #include<bits/stdc++.h>
+// using namespace std;
+// const int MOD = 1e4+7;
 
-int binaryExpoRecur(int a, int b){
-  if(b == 0) return 1;
-  long long ans = binaryExpoRecur(a, b/2) ;
-  if(b & 1) return ((a * (ans * 1LL * ans)) % MOD);
-  else return ((ans * 1LL * ans) % MOD);
-}
+// int binaryExpoRecur(int a, int b){
+//   if(b == 0) return 1;
+//   long long ans = binaryExpoRecur(a, b/2) ;
+//   if(b & 1) return ((a * (ans * 1LL * ans)) % MOD);
+//   else return ((ans * 1LL * ans) % MOD);
+// }
 
 // int binaryExpoIter(int a, int b){
 //   long long ans = 1;
@@ -20,33 +20,58 @@ int binaryExpoRecur(int a, int b){
 // }
 
 // when a <= 10^18 , MOD <= 10^18 
-int binaryExpoIter(long long a, long long b){
-  // a %= MOD;
+// int binaryExpoIter(long long a, long long b){
+//   // a %= MOD;
+//   int ans = 1;
+//   while(b > 0){
+//     if(b & 1) ans = binaryMultiply(ans, a);
+//     a = binaryMultiply(a, a);
+//     b >>= 1;
+//   }
+//   return ans;
+// }
+
+// int binaryMultiply(long long a, long long b){
+//   int ans = 0;
+//   while(b > 0){
+//     if(b & 1) ans = (ans + a) % MOD;
+//     a = (a + a) % MOD;
+//     b >>= 1;
+//   }
+//   return ans;
+// }
+
+// int main(){
+//   int a, b;
+//   cin>>a>>b;
+//   cout<<pow(a, b)<<endl;
+//   cout<<(a<<(b-1))<<endl;
+
+//   cout<<binaryExpoRecur(a, b)<<endl;
+//   cout<<binaryExpoIter(a, b)<<endl;
+// } 
+
+
+
+// when b > 10^18
+#include<bits/stdc++.h>
+using namespace std;
+
+const int M = 1e9+7;
+
+int binaryIter(int a, int b, long long m){
   int ans = 1;
   while(b > 0){
-    if(b & 1) ans = binaryMultiply(ans, a);
-    a = binaryMultiply(a, a);
-    b >>= 1;
-  }
-  return ans;
-}
-
-int binaryMultiply(long long a, long long b){
-  int ans = 0;
-  while(b > 0){
-    if(b & 1) ans = (ans + a) % MOD;
-    a = (a + a) % MOD;
+    if(b & 1) ans = (ans * 1LL * a) % m;
+    a = (a * 1LL * a) % m;
     b >>= 1;
   }
   return ans;
 }
 
 int main(){
-  int a, b;
-  cin>>a>>b;
-  cout<<pow(a, b)<<endl;
-  cout<<(a<<(b-1))<<endl;
-
-  cout<<binaryExpoRecur(a, b)<<endl;
-  cout<<binaryExpoIter(a, b)<<endl;
-} 
+  // a = 50 , b = 64 , c = 32
+  // a^b^c  
+  int a = 50, b = 64, c = 32;
+  cout<<binaryIter(a,binaryIter(b,c,M-1), M)<<endl;
+}
