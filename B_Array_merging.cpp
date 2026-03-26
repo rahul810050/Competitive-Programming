4d979228d55bfec966f486a9b80a9d2310e49ea2
rahul810050 <rahul810050@gmail.com>
2026-03-26 21:17:09 +0530

    cf problems

--- B_Array_merging.cpp
+++ B_Array_merging.cpp
@@ -1,20 +1,20 @@
 // coded by Cocane
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 #define ll long long
-#define forn(i, n) for (int i = 0; i < n; i++)
-#define for1(i, n) for (int i = 1; i <= n; i++)
-#define foru(i, n) for (int i = n - 1; i >= 0; i--)
+#define forn(i, n) for(int i = 0; i < n; i++)
+#define for1(i, n) for(int i = 1; i <= n; i++)
+#define foru(i, n) for(int i = n-1; i >= 0; i--)
 #define fi first
 #define se second
-#define all(x) (x).begin(), (x).end()
-#define allp(x) (x).begin(), (x).begin() + (x).size() / 2, (x).rbegin()
+#define all(x) (x).begin(),(x).end()
+#define allp(x) (x).begin(), (x).begin() + (x).size()/2, (x).rbegin()
 #define pb push_back
 #define ppb pop_back
-#define read(a) for (auto &i : a) cin >> i
-#define out(ans) cout << ans << endl
-#define yn(a) cout << (a ? "YES" : "NO") << endl
+#define read(a) for(auto &i: a) cin >> i
+#define out(ans) cout<<ans<<endl
+#define yn(a) cout<< (a ? "YES": "NO") <<endl
 
 using namespace std;
 
@@ -22,58 +22,76 @@ typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef double dl;
 
-void solve()
-{
-  int n;
-  cin >> n;
-  vi a(n + 1), b(n + 1);
-  for1(i, n) cin >> a[i];
-  for1(i, n) cin >> b[i];
 
-  vi fa(2 * n + 1), fb(2 * n + 1);
-
-  int p = 1;
-  for (int i = 2; i <= n; ++i)
-  {
-    if (a[i] != a[i - 1])
-    {
-      fa[a[i - 1]] = max(fa[a[i - 1]], i - p);
-      p = i;
-    }
-  }
-  fa[a[n]] = max(fa[a[n]], n - p + 1);
-
-  p = 1;
-  for (int i = 2; i <= n; ++i)
-  {
-    if (b[i] != b[i - 1])
-    {
-      fb[b[i - 1]] = max(fb[b[i - 1]], i - p);
-      p = i;
-    }
-  }
-  fb[b[n]] = max(fb[b[n]], n - p + 1);
-
-  int ans = 0;
-  for (int i = 1; i <= 2 * n; ++i)
-  {
-    ans = max(ans, fa[i] + fb[i]);
-  }
-
-  out(ans);
-}
-
-int main()
-{
+int main(){
   ios::sync_with_stdio(false);
   cin.tie(NULL);
 
   int t;
-  cin >> t;
-  while (t--)
-  {
-    solve();
-  }
+  cin>>t;
+  while(t--){
+    ll n; cin>>n;
+    vll a(n), b(n);
+    read(a);
+    read(b);
+    vll c1(2*n+1, 0), c2(2*n+1, 0);
 
+    // ll i = 1, j = 0;
+    // c1.pb(a[0]);
+    // while(i < n && j < n){
+    //   if(c1[i+j] == a[i]){
+    //     c1.pb(a[i++]);
+    //   } else if(c1[i+j] == b[j]){
+    //     c1.pb(b[j++]);
+    //   } else {
+    //     c1.pb(a[i++]);
+    //   }
+    // }
+    // ll mx = 0;
+    // ll ct = 1;
+    // for(ll k = 1; k < 2*n; k++){
+    //   if(c1[k-1] == c1[k]) ct++;
+    //   else ct = 1;
+    //   mx = max(ct,mx);
+    // }
+
+    // i = 0; j = 1;
+    // c2.pb(b[0]);
+    // while(i < n && j < n){
+    //   if(c2[i+j] == a[i]){
+    //     c2.pb(a[i++]);
+    //   } else if(c2[i+j] == b[j]){
+    //     c2.pb(b[j++]);
+    //   } else {
+    //     c2.pb(b[j++]);
+    //   }
+    // }
+    // ct = 1;
+    // for(ll k = 1; k < 2*n; k++){
+    //   if(c2[k-1] == c2[k]) ct++;
+    //   else ct = 1;
+    //   mx = max(ct,mx);
+    // }
+    // out(mx);
+    
+    for(ll i = 0; i < n; ){
+      int j = i;
+      while(j < n && a[j] == a[i]) j++;
+      c1[a[i]] = max(c1[a[i]], j-i);
+      i = j;
+    }
+
+    for(ll i = 0; i < n;){
+      int j = i;
+      while(j < n && b[j] == b[i]) j++;
+      c2[b[i]] = max(c2[b[i]], j-i);
+      i = j;
+    }
+    ll ans = 0;
+    for(ll i = 1; i <= 2*n; i++){
+      ans = max(ans, c1[i]+c2[i]);
+    }
+    out(ans);
+  }
   return 0;
 }
--- B_Monsters.cpp
+++ B_Monsters.cpp
@@ -30,21 +30,41 @@ int main(){
   int t;
   cin>>t;
   while(t--){
-    int n, k;
-    cin >> n >> k;
-    vi a(n);
-    for (auto &x : a) {
-      cin >> x;
-      x %= k;
-      if (!x) x = k;
+    int n, k; cin>>n>>k;
+    vector<pair<ll,int>> v;
+    for(int i = 1; i <= n; i++){
+      ll x; cin>>x;
+      ll y = x % k;
+      if(y == 0) y = k;
+      v.pb({y, i});
     }
-    vi v(n);
-    iota(all(v), 0);
-    stable_sort(all(v), [&](int i, int j) {
-      return a[i] > a[j];
+    
+    // for(auto &it: v){
+    //   cout<<it.first<<it.second<<" ";
+    // } cout<<endl;
+    // sort(all(v));
+    // cout<<"sorted"<<endl;
+    // for(auto &it: v){
+    //   cout<<it.first<<it.second<<" ";
+    // } cout<<endl;
+    // for(auto &it: v){
+    //   cout<<it.second<<" ";
+    // } cout<<endl;
+    // vi order;
+    // for(auto &it: v){
+    //   if(it.first == 0) order.pb(it.second);
+    // }
+    // sort(all(v), greater<pair<ll,int>>());
+    // for(int i = n-1; i >= 0; i--){
+    //   if(v[i].first != 0) order.pb(v[i].second);
+    // }
+    sort(v.begin(), v.end(), [](auto &a, auto &b){
+      if(a.first == b.first)
+        return a.second < b.second; 
+      return a.first > b.first;     
     });
-    for (auto &x : v) cout << x + 1 << ' ';
-    cout << endl;
+    for(auto &it: v) cout<<it.second<<" ";
+    cout<<endl;
   }
   return 0;
 }
